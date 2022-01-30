ae797ab2a62fd4f0e3089f0e29f2701694994bd6
Aishwarya1224 <Aishwarya1224@users.noreply.github.com>
2022-01-30 09:18:08 +0530

    commit

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -55,9 +55,6 @@ const TransactionCard = ({
           <div>
             <p></p>
           </div>
-          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-            <p className="text-[#37c7da] font-bold">{timestamp}</p>
-          </div>
         </div>
       </div>
     </div>
--- client/src/utils/dummyData.js
+++ client/src/utils/dummyData.js
@@ -4,9 +4,9 @@ export default [
     url: "https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284",
     message: "",
     timestamp: "12/21/2021, 4:33:21 PM",
-    addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+    addressFrom: "0xeAD3522F4907F47Fd49cEA2F627F460358925356",
     amount: "0.01",
-    addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
+    addressTo: "0x8d5AAec5531E821c3bD202184a740F9B5b3570C6",
   },
   {
     id: 2,
